6a703bfbe2c692d57c392c2b4b37d26e427dc0bd
davidmonteiro03 <damonteiro12446@gmail.com>
2024-02-21 16:46:08 +0000

    git updates

--- api.py
+++ api.py
@@ -6,16 +6,15 @@
 #    By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/02/21 16:26:48 by dcaetano          #+#    #+#              #
-#    Updated: 2024/02/21 16:35:23 by dcaetano         ###   ########.fr        #
+#    Updated: 2024/02/21 16:45:29 by dcaetano         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import requests
 from bs4 import BeautifulSoup
 
-site = "https://google.pt"
-request = requests.get(site)
-page = open("index.html", "w")
-page.write(f"Status code: {request.status_code}\n")
-page.close()
-request.close()
+def main():
+	print("Hello World!")
+
+if __name__ == "__main__":
+	main()
