9c580d382c6ee6029883ab83de485bb9ba381de9
davidmonteiro03 <damonteiro12446@gmail.com>
2024-02-21 17:49:30 +0000

    git updates

--- api.py
+++ api.py
@@ -6,18 +6,21 @@
 #    By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/02/21 17:13:29 by dcaetano          #+#    #+#              #
-#    Updated: 2024/02/21 17:19:04 by dcaetano         ###   ########.fr        #
+#    Updated: 2024/02/21 17:49:10 by dcaetano         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 from bs4 import BeautifulSoup as bs
 
 def main():
-	file_path = "index.html"
-	file_read = open(file_path, "r")
-	original_conts = file_read.read()
-	file_read.close()
-	print(original_conts, end = "")
+	with open('index.html', 'r') as file:
+		html_content = file.read()
+	soup = bs(html_content, 'html.parser')
+	body_tag = soup.body
+	body_tag.clear()
+	body_tag.append("hello world")
+	with open('index.html', 'w') as file:
+		html_content = file.write(str(soup))
 
 if __name__ == "__main__":
 	main()
