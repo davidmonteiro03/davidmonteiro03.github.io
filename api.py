63e7e55e1e9ab4275c5a7e7cd136b4ed1e9352af
davidmonteiro03 <damonteiro12446@gmail.com>
2024-02-21 18:26:08 +0000

    git updates

--- api.py
+++ api.py
@@ -6,11 +6,24 @@
 #    By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/02/21 17:13:29 by dcaetano          #+#    #+#              #
-#    Updated: 2024/02/21 18:11:41 by dcaetano         ###   ########.fr        #
+#    Updated: 2024/02/21 18:26:01 by dcaetano         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 from bs4 import BeautifulSoup as bs
+
+def create_table(body_tag, soup):
+	table = soup.new_tag("table")
+	rows = 5
+	cols = 3
+	for i in range(1, rows):
+		row = soup.new_tag("tr")
+		for j in range(1, cols):
+			col = soup.new_tag("td")
+			col.string = "value"
+			row.append(col)
+		table.append(row)
+	body_tag.append(table)
 
 def main():
 	with open('index.html', 'r') as file:
@@ -18,8 +31,7 @@ def main():
 	soup = bs(html_content, 'html.parser')
 	body_tag = soup.body
 	body_tag.clear()
-	table = soup.new_tag("table")
-	body_tag.append(table)
+	create_table(body_tag, soup)
 	with open('index.html', 'w') as file:
 		html_content = file.write(str(soup))
 
