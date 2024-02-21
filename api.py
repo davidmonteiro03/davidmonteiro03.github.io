f27b101c83b084be5526c48516f1318d65e2fea1
davidmonteiro03 <damonteiro12446@gmail.com>
2024-02-21 18:34:52 +0000

    git updates

--- api.py
+++ api.py
@@ -6,32 +6,38 @@
 #    By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/02/21 17:13:29 by dcaetano          #+#    #+#              #
-#    Updated: 2024/02/21 18:26:01 by dcaetano         ###   ########.fr        #
+#    Updated: 2024/02/21 18:34:33 by dcaetano         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 from bs4 import BeautifulSoup as bs
 
-def create_table(body_tag, soup):
+def create_table(body, soup):
 	table = soup.new_tag("table")
 	rows = 5
 	cols = 3
 	for i in range(1, rows):
 		row = soup.new_tag("tr")
+		table.append("\n\t\t")
 		for j in range(1, cols):
+			row.append("\n\t\t\t")
 			col = soup.new_tag("td")
 			col.string = "value"
 			row.append(col)
+		row.append("\n\t\t")
 		table.append(row)
-	body_tag.append(table)
+	table.append("\n\t")
+	body.append(table)
 
 def main():
 	with open('index.html', 'r') as file:
 		html_content = file.read()
 	soup = bs(html_content, 'html.parser')
-	body_tag = soup.body
-	body_tag.clear()
-	create_table(body_tag, soup)
+	body = soup.body
+	body.clear()
+	body.append("\n\t")
+	create_table(body, soup)
+	body.append("\n")
 	with open('index.html', 'w') as file:
 		html_content = file.write(str(soup))
 
