5bd340229370b69c526d70fd119c47fea97a5a31
davidmonteiro03 <damonteiro12446@gmail.com>
2024-02-21 19:22:48 +0000

    git updates

--- api.py
+++ api.py
@@ -6,40 +6,49 @@
 #    By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/02/21 17:13:29 by dcaetano          #+#    #+#              #
-#    Updated: 2024/02/21 18:34:33 by dcaetano         ###   ########.fr        #
+#    Updated: 2024/02/21 19:22:23 by dcaetano         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 from bs4 import BeautifulSoup as bs
 
-def create_table(body, soup):
-	table = soup.new_tag("table")
-	rows = 5
-	cols = 3
-	for i in range(1, rows):
-		row = soup.new_tag("tr")
-		table.append("\n\t\t")
-		for j in range(1, cols):
-			row.append("\n\t\t\t")
-			col = soup.new_tag("td")
-			col.string = "value"
-			row.append(col)
-		row.append("\n\t\t")
-		table.append(row)
-	table.append("\n\t")
-	body.append(table)
+def default_page():
+	result = bs(features = "html.parser")
+	html = result.new_tag("html", lang = "en")
+	html.append("\n\t")
+	head = result.new_tag("head")
+	head.append("\n\t\t")
+	lang = result.new_tag("meta", charset = "utf-8")
+	design = result.new_tag("meta", content = "width=device-width, initial-scale=1.0", attrs = { "name": "viewport" })
+	styles = result.new_tag("link", rel = "stylesheet", attrs = { "href": "styles/home.css" })
+	title = result.new_tag("title")
+	title.string = "David Monteiro - Homepage"
+	head.append(lang)
+	head.append("\n\t\t")
+	head.append(design)
+	head.append("\n\t\t")
+	head.append(styles)
+	head.append("\n\t\t")
+	head.append(title)
+	head.append("\n\t")
+	html.append(head)
+	body = result.new_tag("body")
+	html.append("\n\t")
+	h1 = result.new_tag("h1")
+	body.append("\n\t\t")
+	h1.string = "HELLO WORLD!"
+	body.append(h1)
+	body.append("\n\t")
+	html.append(body)
+	html.append("\n")
+	result.append(html)
+	return (result)
 
 def main():
-	with open('index.html', 'r') as file:
-		html_content = file.read()
-	soup = bs(html_content, 'html.parser')
-	body = soup.body
-	body.clear()
-	body.append("\n\t")
-	create_table(body, soup)
-	body.append("\n")
-	with open('index.html', 'w') as file:
-		html_content = file.write(str(soup))
+	doctype = "<!DOCTYPE html>"
+	default = default_page()
+	with open("index.html", "w") as file:
+		file.write(doctype + "\n" + str(default) + "\n")
 
 if __name__ == "__main__":
 	main()
