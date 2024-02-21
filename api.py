9a04ce2b485d25cf5fe4417b351db006677f818a
davidmonteiro03 <damonteiro12446@gmail.com>
2024-02-21 19:48:11 +0000

    git updates

--- api.py
+++ api.py
@@ -6,7 +6,7 @@
 #    By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/02/21 17:13:29 by dcaetano          #+#    #+#              #
-#    Updated: 2024/02/21 19:46:40 by dcaetano         ###   ########.fr        #
+#    Updated: 2024/02/21 19:48:10 by dcaetano         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -50,19 +50,21 @@ def default_page():
 def get_table():
 	site = "https://desporto.sapo.pt/futebol/competicao/primeira-liga-2/classificacao"
 	request = requests.get(site)
-	if (request.status_code == 200):
-		html_content = request.content
-		soup = bs(html_content, "html.parser")
-		table = soup.find("table", class_ = "[ ink-table medium bottom-space ] rankings-table")
-		if (table != None):
-			rows = table.find_all("tr")
-			if (len(rows) > 0):
-				for row in rows:
-					cols = row.find_all("td")
-					if (len(cols) > 0):
-						for col in cols:
-							print(col.text, end = " ")
-						print()
+	if (request.status_code != 200):
+		return None
+	html_content = request.content
+	soup = bs(html_content, "html.parser")
+	table = soup.find("table", class_ = "[ ink-table medium bottom-space ] rankings-table")
+	if (table == None):
+		return None
+	rows = table.find_all("tr")
+	if (len(rows) > 0):
+		for row in rows:
+			cols = row.find_all("td")
+			if (len(cols) > 0):
+				for col in cols:
+					print(col.text, end = " ")
+				print()
 
 def main():
 	doctype = "<!DOCTYPE html>"
