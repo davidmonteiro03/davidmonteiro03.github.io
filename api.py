3b3f7b9fe504e974fb21a956a3d23c7cbdc70438
davidmonteiro03 <damonteiro12446@gmail.com>
2024-02-21 17:19:47 +0000

    git updates

--- api.py
+++ api.py
@@ -5,16 +5,19 @@
 #                                                     +:+ +:+         +:+      #
 #    By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2024/02/21 16:26:48 by dcaetano          #+#    #+#              #
-#    Updated: 2024/02/21 16:45:29 by dcaetano         ###   ########.fr        #
+#    Created: 2024/02/21 17:13:29 by dcaetano          #+#    #+#              #
+#    Updated: 2024/02/21 17:19:04 by dcaetano         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-import requests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
 
 def main():
-	print("Hello World!")
+	file_path = "index.html"
+	file_read = open(file_path, "r")
+	original_conts = file_read.read()
+	file_read.close()
+	print(original_conts, end = "")
 
 if __name__ == "__main__":
 	main()
