4609301f90a1169a07c0a9b9c89c738a3c6752a9
davidmonteiro03 <damonteiro12446@gmail.com>
2024-02-21 17:58:19 +0000

    git updates

--- api.py
+++ api.py
@@ -6,11 +6,15 @@
 #    By: dcaetano <dcaetano@student.42porto.com>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/02/21 17:13:29 by dcaetano          #+#    #+#              #
-#    Updated: 2024/02/21 17:52:10 by dcaetano         ###   ########.fr        #
+#    Updated: 2024/02/21 17:58:06 by dcaetano         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 from bs4 import BeautifulSoup as bs
+
+def get_table():
+	result = "table"
+	return result
 
 def main():
 	with open('index.html', 'r') as file:
@@ -18,7 +22,9 @@ def main():
 	soup = bs(html_content, 'html.parser')
 	body_tag = soup.body
 	body_tag.clear()
-	body_tag.append("\n\thello worlds\n")
+	body_tag.append("\n\t")
+	body_tag.append(get_table())
+	body_tag.append("\n")
 	with open('index.html', 'w') as file:
 		html_content = file.write(str(soup))
 
